ae41fb4ed832fb2af0284e3efaa52298947f64a2
zuh2407 <zuhair2407atham@gmail.com>
2025-11-27 17:08:39 +0400

    Improve UI, fix PDF invoice, and cleanup unused files

--- vercel_app.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmacy.settings')
-
-application = get_wsgi_application()
-app = application
